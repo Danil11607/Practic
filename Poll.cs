41635371a0d38df1bd480c1fec5b83d8acc952ac
EvgenyTerekhin_11-608 <evgter55@gmail.com>
2017-07-07 00:52:22 +0300

    first commit

---
+++ Poll.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iter1
+{
+	class Poll
+	{
+		int numberOfQuestions;
+		string tag;
+		bool asked = false;
+
+		Poll(string tag, int numberOfQuestions)
+		{
+			this.numberOfQuestions = numberOfQuestions;
+			this.tag = tag;
+			Question[] questionArr = new Question[numberOfQuestions];
+			for (int i = 0; i < numberOfQuestions; i++)
+			{
+				Question questionTemp = new Question(/*parse*/);
+				questionArr[i] = questionTemp;
+			}
+
+		}
+	}
+
+	class Answer
+	{
+		enum answerType { single, multi, textSingle, textMulti };
+		string answer;
+		answerType type = new answerType();
+		int skipTo;// убрать все пропуски
+
+		Answer(string answer, answerType type, int skipTo = 0)
+		{
+			this.answer = answer;
+			this.type = type;
+			this.skipTo = skipTo;// убрать все пропуски
+		}
+	}
+
+	class Question
+	{
+		string question;
+		int numberOfAnswers;
+		bool asked = false;
+
+		Question(string question, int numberOfAnswers)
+		{
+			this.question = question;
+			this.numberOfAnswers = numberOfAnswers;
+			Answer[] answerArr = new Answer[numberOfAnswers];
+			for (int i = 0; i < numberOfAnswers; i++)
+			{
+				Answer answerTemp = new Answer(/*parse*/);
+				answerArr[i] = answerTemp;
+			}
+		}
+	}
+}
