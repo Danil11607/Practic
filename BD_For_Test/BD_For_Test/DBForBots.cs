f2bf4b07ab300a755f702e05f4fae740dd110426
EvgenyTerekhin_11-608 <evgter55@gmail.com>
2017-07-07 16:01:20 +0300

    first commit

---
+++ BD_For_Test/BD_For_Test/DBForBots.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PollStruct;
+
+
+using System.Data.Entity;
+
+namespace BD_For_Test
+{
+	class CreateBDForQuestions
+	{
+		Poll pl;
+		class UserContext : DbContext
+		{
+			public UserContext()
+				: base("DbConnection")
+			{ }
+
+			public DbSet<Node> DBQuestions { get; set; }
+		}
+		public CreateBDForQuestions(Poll pl)
+		{
+			this.pl = pl;
+        }
+		
+		public void create()
+		{
+			Question que;
+			List<PollStruct.Answer> answer=new List<Answer>();
+			using (UserContext DB = new UserContext())
+			{
+
+				for (int i = 0; i < pl.Questions.Count; i++)
+				{
+					que = pl.Questions[i];
+					for (int j = 0; j < pl.Questions[i].Answers.Count; j++)
+					{
+						answer.Add(pl.Questions[i].Answers[j]);
+						
+					}
+					Node nd = new Node { que = que, Answers = answer };
+					DB.DBQuestions.Add(nd);
+					DB.SaveChanges();
+					answer = null;
+				}	
+			}
+        }
+	}
+	class Node
+	{
+		public int Id { get; set; }
+		public Question que { get; set; }
+		public List<PollStruct.Answer> Answers { get; set; }
+	}
+	
+	
+	class DBForBots
+	{
+		static void Main(string[] args)
+		{
+			PollReader pr = new PollReader();
+			Poll poll = pr.ReadPoll();
+			CreateBDForQuestions BD = new CreateBDForQuestions(poll);
+			BD.create();
+
+		}
+	}
+}
+
+
---
+++ BD_For_Test/BD_For_Test/PollStruct.cs
@@ -0,0 +1,52 @@
+﻿using BD_For_Test;
+using System.Collections.Generic;
+
+namespace PollStruct
+{
+	class Poll
+	{
+		public enum pollType { anon, halfanon, nonanon };
+		public pollType type;
+		private int count;
+		public string token;
+		public string password;
+		public List<Question> Questions = new List<Question>();
+
+		public Poll(string token, int count, string password, pollType type)
+		{
+			this.count = count;
+			this.token = token;
+			this.type = type;
+		}
+	}
+
+	class Answer
+	{
+		public int Id { get; set; }
+		public string Content;
+		public Question skipTo;// убрать все пропуски
+		public Node nd;
+
+		public Answer(string answer, Question skipTo = null)
+		{
+			this.Content = answer;
+			this.skipTo = skipTo;// убрать все пропуски
+		}
+	}
+
+	class Question
+	{
+		public enum questionType { test, testMulti, testSkip, question };
+		public questionType type;
+		public string content;
+		public int count;
+		public List<Answer> Answers=new List<Answer>();
+
+		public Question(string content, questionType type, int count)
+		{
+			this.content = content;
+			this.type = type;
+			this.count = count;
+		}
+	}
+}
