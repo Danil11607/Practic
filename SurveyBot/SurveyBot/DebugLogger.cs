15f387a6aff377e807663fd66640c1b7abc8f6d7
Евгений Корчагин <ESKorchagin@stud.kpfu.ru>
2017-07-07 07:33:23 +0300

    main bot+ debuger

---
+++ SurveyBot/SurveyBot/DebugLogger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Telegram.Bot;
+
+namespace Debug
+{
+    public class DebugMessage
+    {
+        public enum MessageLevel { Error, Debug, Info }
+        public static void SendDebug(string ClientName, string Text, MessageLevel Level)
+        {
+            switch (Level)
+            {
+                case MessageLevel.Debug:
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    break;
+                case MessageLevel.Error:
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    break;
+                default:
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+            }
+            Console.WriteLine($"[Time:{DateTime.Now}] [{ClientName}] [{Level.ToString()}]:{Text}");
+        }
+    }
+}
---
+++ SurveyBot/SurveyBot/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using System.Threading;
+using Debug;
+
+namespace SurveyBot
+{
+    class Program
+    {
+        static TelegramBotClient Bot = new TelegramBotClient("343759311:AAGwKYHxEX4kC2DvLlXpKovytJKCDq1Inwk");
+        static void Main(string[] args)
+        {
+            Bot.StartReceiving();
+            Bot.OnMessage += MessageParse;
+            Console.ReadKey(); 
+        }
+
+        private static void MessageParse(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        {
+            string[] msg = e.Message.Text.Split(' ');
+            switch (msg[0])
+            {
+                case "/join":
+                    if (msg.Count() != 2)
+                    {
+                        DebugMessage.SendDebug("MainClient", "Wrong Arguments", DebugMessage.MessageLevel.Error);
+                        Bot.SendTextMessageAsync(e.Message.Chat.Id, "Wrong Arguments");
+                    }
+                    break;
+            }
+        }
+    }
+}
