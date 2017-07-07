1db6506cfe5264868fdb306c6b813eabc63f64c5
EvgenyTerekhin_11-608 <evgter55@gmail.com>
2017-07-07 13:51:45 +0300

    first commit

---
+++ DBForBots.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PollStruct;
+using System.Data.Entity;
+namespace BD_For_Test
+{
+	class CreateBDForQuestions
+	{
+		Poll pl;
+		class UserContext : DbContext
+		{
+			public UserContext()
+				: base("DbConnection")
+			{ }
+
+			public DbSet<Node> DBQuestions { get; set; }
+		}
+		public CreateBDForQuestions(Poll pl)
+		{
+			this.pl = pl;
+        }
+		
+		public void  create()
+		{
+			Question que;
+			List<PollStruct.Answer> answer=null;
+			using (UserContext DB = new UserContext())
+			{
+
+				for (int i = 0; i < pl.Questions.Count; i++)
+				{
+					que = pl.Questions[i];
+					for (int j = 0; j < pl.Questions[i].Answers.Count; j++)
+					{
+						answer.Add(pl.Questions[i].Answers[j]);
+						
+					}
+					DB.DBQuestions.Add(new Node {que=que,Answers=answer});
+					DB.SaveChanges();
+					answer = null;
+				}	
+			}
+        }
+	}
+	
+	class Node
+	{
+	    public Question que;
+		public List<PollStruct.Answer> Answers;
+	}
+	
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+		}
+	}
+}
+
+
